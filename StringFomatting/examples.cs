f7a40a2bf1b6da2aa760ad5d9444726447122fe3
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-09-24 15:14:32 -0400

    Update examples.cs

--- StringFomatting/examples.cs
+++ StringFomatting/examples.cs
@@ -35,3 +35,18 @@ Console.WriteLine(message);
 string projectName = "First-Project";
 Console.WriteLine($@"C:\Output\{projectName}\Data");
 
+//section challenge
+/*output will be
+View English output:
+    c:\Excercise\ACME\data.txt
+Посмотреть русский вывод:
+    c:\Excercise\ACME\ru-RU\data.txt
+*/
+string projectName = "ACME";
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+
+Console.WriteLine(@"View English output:
+    c:\Excercise\"+projectName+@"\data.txt");
+Console.WriteLine($@"{russianMessage}:
+    c:\Excercise\{projectName}\ru-RU\data.txt");
