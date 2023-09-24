d17c90856a84b21c320ae5665b376f062ce9d4e0
Robert Thompson <95152169+RoberThompson@users.noreply.github.com>
2023-09-24 14:42:41 -0400

    Update Verbatim_and_Unicode.cs

--- StringFomatting/Verbatim_and_Unicode.cs
+++ StringFomatting/Verbatim_and_Unicode.cs
@@ -1,3 +1,4 @@
+
 Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
 Console.WriteLine("Invoice: 1021\t\tComplete!");
 Console.WriteLine("Invoice: 1022\t\tComplete!");
@@ -8,3 +9,16 @@ Console.Write(@"c:\invoices");//verbatim. no need to escape char/
 Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
 //user command to run an application. \u for unicode
 Console.WriteLine(@"c:\invoices\app.exe -j");
+
+//will output to console
+/*Generating invoices for customer "Contoso Corp" ...
+
+Invoice: 1021		Complete!
+Invoice: 1022		Complete!
+
+Output Directory:	
+c:\invoices
+
+日本の請求書を生成するには：
+	c:\invoices\app.exe -j
+*/
